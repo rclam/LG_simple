ebc483daad2eafb8cdc62975be71cbffb35677f6
R.C. Lam <75513951+rclam@users.noreply.github.com>
2022-04-21 13:13:05 -0500

    Update a_cleanData.py

--- a_cleanData.py
+++ a_cleanData.py
@@ -3,9 +3,9 @@ import numpy as np
 from paraview.simple import *
 import clean2grid
 
-config = 2991
+config = 0
 
-for c in range(134):
+for c in range(695):
     step=0
     for ts in range(19):
         print('\nc: ', config)
