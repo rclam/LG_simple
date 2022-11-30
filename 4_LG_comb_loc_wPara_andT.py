bc54ab86bd46cab887dadbd158d0f2128e1dafd2
asaxena <saxena.arushi@ufl.edu>
2022-11-30 14:53:28 -0500

    Fix the artifact.

--- 4_LG_comb_loc_wPara_andT.py
+++ 4_LG_comb_loc_wPara_andT.py
@@ -36,10 +36,10 @@ inp_locs = np.vstack((a_x_loc_sc, a_y_loc_sc)).T # combine co-ordinate data (her
 
 a_par_rep = a_par_sc[:,None,:].repeat(3201,1) # repeat parameters (scaled) for each of the 3201 points
 
-a_new_par = np.zeros((3124, 3201, 7)) # initialize new data array for 5 parameters columns and 2 co-ordinate columns per data point per time step
+a_new_par = np.zeros((626, 3201, 7)) # initialize new data array for 5 parameters columns and 2 co-ordinate columns per data point per time step
 
 # fill new data array with original parameters
-for i in range( 3124):
+for i in range( 626):
     a_new_par[i,:,0] = a_par_sc[i,0].repeat(3201)
     a_new_par[i,:,1] = a_par_sc[i,1].repeat(3201)
     a_new_par[i,:,2] = a_par_sc[i,2].repeat(3201)
@@ -53,9 +53,13 @@ a_new_par[:,:,6] = inp_locs[:,1]
 # np.save('a_par_w_locs_sc.npy', a_new_par). # can save as is if desire
 
 print('Loading temperature array...')
-a_T = np.load("a_T_clean.npy")
+a_T = np.load("a_T_clean_1.npy")
 print('Scaling data...')
-a_T_sc = scaler.fit_transform(a_T.reshape(-1, a_T.shape[-1])).reshape(a_T.shape)
+print( a_T.shape[-1], a_T.shape, a_x_loc.shape)
+# the first reshape makes the 3D to 2D array with same enteries
+a_transform = (a_T.reshape(-1, a_T.shape[-1])).T
+a_T_sc1 = (scaler.fit_transform(a_transform)).T
+a_T_sc  = a_T_sc1.reshape(a_T.shape)
 
 
 # print('\nLoading parameters array...') # if already saved new parameter array (without temperature data), can load here
@@ -68,16 +72,18 @@ a_par_expand = a_new_par[:,None,:].repeat(18,1)
 
 
 print('\nCreating X array...')     # initialize new X array
-X = np.zeros((3124, 18, 3201, 8))
+X = np.zeros((626, 18, 3201, 8))
 
 
 a_T_x = a_T_sc[:,:-1,:]                                 # keep all but last time step data
-a_T_trial = a_T_x.reshape(3124, 18, 3201, 1)            # reshape for CNN input
+print (a_T_x.shape)
+#a_T_trial = a_T_x.reshape(3124, 18, 3201, 1)            # reshape for CNN input
+a_T_trial = a_T_x.reshape(626, 18, 3201, 1)            # reshape for CNN input
 X = np.concatenate((a_par_expand, a_T_trial), axis=3)   # combine parameter array and temperature array
 
 print('\nStoring last time step T data into y...')
-# y = a_T_sc[:,-1,:]       # Target label = last time step
-y = a_T[:,-1,:]       # Target label = last time step
+y = a_T_sc[:,-1,:]       # Target label = last time step
+#y = a_T[:,-1,:]       # Target label = last time step
 
-# np.save('a_par_loc_T_sc.npy', X) # save files
-# np.save('a_y_sc.npy', y)
+np.save('a_par_loc_T_sc.npy', X) # save files
+np.save('a_y_sc.npy', y)
